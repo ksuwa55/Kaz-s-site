84c4eb08a21e6e03417defe53a5062b89c82e6c0
kazuki <klklklkl063055@gmail.com>
2023-12-27 13:11:44 +0900

    add scroll feature from footer to top

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -102,3 +102,17 @@
     data: data,
     options: options,
   });
+
+  // footer
+  const returnTop = () => {
+    const scrollDuration = 500;
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+
+    const scrollInterval = setInterval(() => {
+      if(window.scrollY !== 0){
+        window.scrollBy(0, scrollStep);
+      }else{
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  };
