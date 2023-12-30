ec25bcb45a7d8aac1ae677168496775349e36a2d
kazuki <klklklkl063055@gmail.com>
2023-12-30 23:56:17 +0900

    change scroll behavior to smooth

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -102,17 +102,3 @@
     data: data,
     options: options,
   });
-
-  // footer
-  const returnTop = () => {
-    const scrollDuration = 800;
-    const scrollStep = -window.scrollY / (scrollDuration / 15);
-
-    const scrollInterval = setInterval(() => {
-      if(window.scrollY !== 0){
-        window.scrollBy(0, scrollStep);
-      }else{
-        clearInterval(scrollInterval);
-      }
-    }, 15);
-  };
